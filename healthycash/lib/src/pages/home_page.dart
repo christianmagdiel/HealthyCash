af63369370bb27177fc3edeeb2faaa2be9629790
cristian alvarado esperano <Cris_ales@LIVE.COM.MX>
2020-02-20 17:59:33 -0700

    Se agrega conexion con Firebase

--- healthycash/lib/src/pages/home_page.dart
+++ healthycash/lib/src/pages/home_page.dart
@@ -1,7 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:healthycash/src/widgets/bottomAppBar_widget.dart';
-import 'package:healthycash/src/widgets/graph_widget.dart';
+import 'package:healthycash/src/widgets/month_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,76 +12,56 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   PageController _controller;
   int currentPage = 9;
+  Stream<QuerySnapshot> _query;
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
+
+    _query = Firestore.instance
+        .collection('expenses')
+        .where("month", isEqualTo: currentPage + 1)
+        .snapshots();
+
     _controller = PageController(
       initialPage: currentPage,
-      viewportFraction: 0.4
+      viewportFraction: 0.4,
     );
   }
+
+  Widget _bottomAction(IconData icon) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(icon),
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        brightness: Brightness.dark,
-        centerTitle: true,
-        title: Row(
-          children: <Widget>[
-            Icon(Icons.monetization_on,
-                size: 30, color: Theme.of(context).primaryColor),
-            SizedBox(width: 10),
-            Text('HealthyCash',
-                style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold))
-          ],
-        ),
-        actions: <Widget>[
-          PopupMenuButton(
-            icon: Icon(Icons.more_vert, color: Colors.black),
-            onSelected: (String value) {
-              if (value == "exit") {
-                print('Exit');
-              }
-            },
-            itemBuilder: (context) => [
-              PopupMenuItem(
-                value: "share",
-                child: Text('Share App'),
-              ),
-              PopupMenuItem(
-                value: "exit",
-                child: Text('Exit App'),
-              )
-            ],
-          )
-        ],
-      ),
       bottomNavigationBar: BottomAppBar(
-        clipBehavior: Clip.antiAlias,
         notchMargin: 8.0,
         shape: CircularNotchedRectangle(),
         child: Row(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            bottomAction(FontAwesomeIcons.history),
-            bottomAction(FontAwesomeIcons.chartPie),
+            _bottomAction(FontAwesomeIcons.history),
+            _bottomAction(FontAwesomeIcons.chartPie),
             SizedBox(width: 48.0),
-            bottomAction(FontAwesomeIcons.wallet),
-            bottomAction(Icons.settings)
+            _bottomAction(FontAwesomeIcons.wallet),
+            _bottomAction(Icons.settings),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-      //backgroundColor: Theme.of(context).primaryColor,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
       body: _body(),
     );
   }
@@ -90,22 +71,30 @@ class _HomePageState extends State<HomePage> {
       child: Column(
         children: <Widget>[
           _selector(),
-          _expenses(),
-          _graph(),
-          Container(
-            color: Colors.blueAccent.withOpacity(0.15),
-            height: 14.0,
+          StreamBuilder<QuerySnapshot>(
+            stream: _query,
+            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> data) {
+              if (data.hasData) {
+                return MonthWidget(
+                  documents: data.data.documents,
+                );
+              } else {
+                return Center(
+                  child: CupertinoActivityIndicator(
+                    radius: 20.0,
+                  ),
+                );
+              }
+            },
           ),
-          _list(),
         ],
       ),
     );
   }
 
-  Widget _pageItem(String name, int position){
+  Widget _pageItem(String name, int position) {
     var _alignment;
-
-     final selected = TextStyle(
+    final selected = TextStyle(
       fontSize: 20.0,
       fontWeight: FontWeight.bold,
       color: Colors.blueGrey,
@@ -126,7 +115,8 @@ class _HomePageState extends State<HomePage> {
 
     return Align(
       alignment: _alignment,
-      child: Text(name,
+      child: Text(
+        name,
         style: position == currentPage ? selected : unselected,
       ),
     );
@@ -134,15 +124,19 @@ class _HomePageState extends State<HomePage> {
 
   Widget _selector() {
     return SizedBox.fromSize(
-        size: Size.fromHeight(30.0),
-        child: PageView(
-          onPageChanged: (newPage){
-            setState(() {
-              currentPage = newPage;
-            });
-          },
-          controller: _controller,
-          children: <Widget>[
+      size: Size.fromHeight(70.0),
+      child: PageView(
+        onPageChanged: (newPage) {
+          setState(() {
+            currentPage = newPage;
+            _query = Firestore.instance
+                .collection('expenses')
+                .where("month", isEqualTo: currentPage + 1)
+                .snapshots();
+          });
+        },
+        controller: _controller,
+        children: <Widget>[
           _pageItem("Enero", 0),
           _pageItem("Febrero", 1),
           _pageItem("Marzo", 2),
@@ -155,68 +149,7 @@ class _HomePageState extends State<HomePage> {
           _pageItem("Octubre", 9),
           _pageItem("Noviembre", 10),
           _pageItem("Diciembre", 11),
-          ],
-        ));
-  }
-
-  Widget _expenses() {
-    return Column(
-      children: <Widget>[
-        Text(
-          "\$2000.25",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40.0),
-        ),
-        Text(
-          'Gasto total',
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 16.0,
-              color: Colors.blueGrey),
-        )
-      ],
-    );
-  }
-
-  Widget _graph() {
-    return Container(height: 250.0, child: GraphWidget());
-  }
-
-  Widget _item(IconData icon, String nombre, int percent, double value) {
-    return ListTile(
-      leading: Icon(icon),
-      title: Text(nombre,
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
-      subtitle: Text("$percent% of expenses",
-          style: TextStyle(fontSize: 16.0, color: Colors.blueGrey)),
-      trailing: Container(
-          decoration: BoxDecoration(
-              color: Colors.blueAccent.withOpacity(0.2),
-              borderRadius: BorderRadius.circular(5.0)),
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              '\$$value',
-              style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 18.0),
-            ),
-          )),
-    );
-  }
-
-  Widget _list() {
-    return Expanded(
-      child: ListView.separated(
-        itemCount: 15,
-        itemBuilder: (BuildContext context, int index) =>
-            _item(FontAwesomeIcons.shoppingCart, "Shopping", 14, 145.12),
-        separatorBuilder: (BuildContext context, int index) {
-          return Container(
-            color: Colors.blueAccent.withOpacity(0.15),
-            height: 3.0,
-          );
-        },
+        ],
       ),
     );
   }
